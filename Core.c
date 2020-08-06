0ca25d79764598c3c39e1ba4b611ed9810c911a7
ajudge026 <67866986+ajudge026@users.noreply.github.com>
2020-08-06 11:07:23 -0400

    Add files via upload

--- Core.c
+++ Core.c
@@ -121,8 +121,10 @@ bool tickFunc(Core *core)
 
     Signal shifted_immediate = ShiftLeft1(ImmeGen(input));
 	printf("the non shifted immediate is - %ld\n", ImmeGen(input));
-	printf("the sifted immediate is - %ld\n", (zero_alu_input && signals.Branch));
-	printf("the mux input is - %ld\n", shifted_immediate);
+	printf("the mux input is - %ld\n", (zero_alu_input && signals.Branch));
+	printf("the zero_alu_input is - %ld\n", zero_alu_input );
+	printf("signals.Branch is - %ld\n",  signals.Branch);
+	printf("the shifted immediate is  - %ld\n", shifted_immediate);
     core->PC = Add(core->PC, MUX((zero_alu_input & signals.Branch), 4, (signed int)shifted_immediate));
 	
     printf(" Program Counter: %ld\n", core->PC);
