b28e56f1fe6a74b85191532a1ad316c2f55da981
ajudge026 <67866986+ajudge026@users.noreply.github.com>
2020-08-09 07:00:01 -0400

    Add files via upload

--- Core.c
+++ Core.c
@@ -126,12 +126,12 @@ bool tickFunc(Core *core)
 	//printf("the instruction  is - %d\n",instruction );
 	//printf("the opcode is - %d\n",(instruction & 127));
 	
-	/* if (core->PC == 28)
+	 if (core->PC == 28)
 	{
 		printf("+++++++++++++<<<x32>>>+++++++++++++++++++++++++++++ the value of x%ld after adding to  %ld and %ld is - %ld\n", write_reg, read_reg_1_value, read_reg_2_value, ALU_output );
 		printf("+++++++++++++<<<x32>>>+++++++++++++++++++++++++++++ the instruction is -  %d\n", instruction);
 
-	} */
+	} 
 	/* if (core->PC == 0)
 	{
 		printf("+++++++++++++<<<x0>>>+++++++++++++++++++++++++++++ the value of x%ld after adding to  %ld and %ld is - %ld\n", write_reg, read_reg_1_value, read_reg_2_value, ALU_output );
